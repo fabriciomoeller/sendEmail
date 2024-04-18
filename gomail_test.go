fb963217b3cae883150901c6784573cdb90653b0
fabriciomoeller <fabriciomoeller@gmail.com>
2024-04-17 21:25:00 -0300

    Teste de email sequencial levou 7.891s

--- gomail_test.go
+++ gomail_test.go
@@ -4,51 +4,56 @@ import (
 	"testing"
 )
 
+// Variáveis de teste. Pode ser alterado para testar diferentes cenários.
+var tests = []struct {
+	to      string
+	from    string
+	subject string
+	body    string
+	error   string
+}{
+	{
+		to:      "fabricio@envioemail.com",
+		from:    "fabricio.moeller@datainfo.inf.br",
+		subject: "Teste de email com sucesso",
+		body:    "Email enviado por teste unitário",
+	},
+	{
+		to:      "",
+		from:    "fabricio.moeller@datainfo.inf.br",
+		subject: "Teste de email com erro no TO",
+		body:    "Email enviado por teste unitário",
+		error:   "Erro esperado no TO",
+	},
+	{
+		to:      "fabricio@envioemail.com",
+		from:    "",
+		subject: "Teste de email com erro no FROM",
+		body:    "Email enviado por teste unitário",
+		error:   "Erro esperado no FROM",
+	},
+}
+
 // TestSendEmail testa a função sendEmail.
 
 func TestSendEmail(t *testing.T) {
-	sucesso, err := sendEmail(
-		"fabricio@envioemail.comr",
-		"fabricio.moeller@datainfo.inf.br",
-		"Teste de email 2",
-		"Email enviado por teste unitário")
-	if err != nil {
-		t.Fatal(err)
-	} else {
-		t.Log(sucesso)
-	}
-}
 
-func TestSendEmailEmptyTo(t *testing.T) {
-	sucesso, err := sendEmail(
-		"",
-		"fabricio.moeller@datainfo.inf.br",
-		"Teste de email 2",
-		"Email enviado por teste unitário")
-	if err != nil {
-		if err.Error() != `gomail: could not send email 1: gomail: invalid address "": mail: no address` {
-			t.Fatal("Gerou o ERRO")
-		} else {
-			t.Log("Erro esperado no TO: ", err)
-		}
-	} else {
-		t.Log(sucesso)
-	}
-}
+	for _, test := range tests {
 
-func TestSendEmailEmptyFrom(t *testing.T) {
-	sucesso, err := sendEmail(
-		"fabricio@envioemail.com",
-		"",
-		"Teste de email 2",
-		"Email enviado por teste unitário")
-	if err != nil {
-		if err.Error() != `gomail: could not send email 1: gomail: invalid address "": mail: no address` {
-			t.Fatal(err)
+		sucesso, err := sendEmail(
+			test.from,
+			test.to,
+			test.subject,
+			test.body)
+		if err != nil {
+			if err.Error() != `gomail: could not send email 1: gomail: invalid address "": mail: no address` {
+				t.Fatal("Gerou o ERRO")
+			} else {
+				t.Log(test.error, err)
+			}
 		} else {
-			t.Log("Erro esperado no FROM: ", err)
+			t.Log(sucesso)
 		}
-	} else {
-		t.Log(sucesso)
+
 	}
 }
